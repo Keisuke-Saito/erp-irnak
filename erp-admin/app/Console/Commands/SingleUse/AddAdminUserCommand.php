f1684fe67689dd7a311f1fed0399d7fefd7f8a3e
Keisuke Saito <j27e4dd7c5577v3@gmail.com>
2024-09-18 04:00:06 +0900

    first commit

---
+++ erp-admin/app/Console/Commands/SingleUse/AddAdminUserCommand.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Console\Commands\SingleUse;
+
+use Illuminate\Console\Command;
+
+// /**
+//  * 管理者登録
+//  *
+//  * @property string $signature
+//  * @property string $description
+//  */
+// class AddAdminUserCommand extends Command
+// {
+//     /** @var string コマンド */
+//     protected $signature = '';
+//     /** @var string コマンド詳細 */
+//     protected $description = '';
+
+//     /**
+//      * Create a new command instance.
+//      *
+//      * @return void
+//      */
+//     public function __construct()
+//     {
+//     }
+
+//     /**
+//      * Execute the console command.
+//      *
+//      * @return void
+//      */
+//     public function handle()
+//     {
+//     }
+// }
