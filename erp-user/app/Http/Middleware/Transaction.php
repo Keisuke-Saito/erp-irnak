0af4ee4ff60215cca148d5a43648673e3430e3ea
Keisuke Saito <j27e4dd7c5577v3@gmail.com>
2024-09-20 00:20:43 +0900

    transaction課題

--- erp-user/app/Http/Middleware/Transaction.php
+++ erp-user/app/Http/Middleware/Transaction.php
@@ -10,17 +10,21 @@ class Transaction
 {
     public function handle($request, Closure $next)
     {
-        \DB::beginTransaction();
+        if (!$request->isMethod("get")) {
+            \DB::beginTransaction();
+        }
         $response = $next($request);
-        if ($response instanceof BinaryFileResponse) {
-            \DB::commit();
-        } elseif (
-            $response->exception
-            && !$response->exception instanceof PackageValidationException
-            && !$response->exception instanceof ValidationException) {
-            \DB::rollBack();
-        } else {
-            \DB::commit();
+        if (!$request->isMethod("get")) {
+            if ($response instanceof BinaryFileResponse) {
+                \DB::commit();
+            } elseif (
+                $response->exception
+                && !$response->exception instanceof PackageValidationException
+                && !$response->exception instanceof ValidationException) {
+                \DB::rollBack();
+            } else {
+                \DB::commit();
+            }
         }
         return $response;
     }
